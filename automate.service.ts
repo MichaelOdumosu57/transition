0cca1bec54b724fb343cf37382fb1f214340cf45
Michael Odumosu <21322737+MichaelOdumosu57@users.noreply.github.com>
2022-12-06 17:04:08 -0500

    Update automate.service.ts

--- automate.service.ts
+++ automate.service.ts
@@ -1,38 +1,7 @@
-  generateFile(counter = 0){
-    let obj = { hello: "world "+ counter };
-    let blob =  new File([JSON.stringify(obj)], "foo.json", {
-      type: "application/json",
-    });
-    return blob
+  documentQuerySelector(selector:string){
+    return document.querySelector(selector) as HTMLElement
   }
 
-  traverseClassAndNoopAllAutomationInit(){
-    let entries = Object.entries(this)
-    .filter((entry)=>{
-      let [key,val]= entry
-      return val.constructor.name === "Object"
-    })
-
-    let result = Object.fromEntries(entries)
-
-    this.traverseClassAndNoopAllAutomation(result)
-  }
-
-  traverseClassAndNoopAllAutomation(obj,stack=[]){
-    if(!environment.production) {
-      return
-    }
-    Object.entries(obj).forEach(entry=>{
-      let [key,value] = entry
-      if(value.constructor.name === "Object"){
-        stack.push(obj[key])
-        this.traverseClassAndNoopAllAutomation(value,stack)
-        stack = []
-      }
-      else{
-        if(value instanceof Function){
-          stack[stack.length-1][key] = ()=>{}
-        }
-      }
-    })
+  documentQuerySelectorAll(selector:string){
+    return Array.from(document.querySelectorAll(selector)) as Array<HTMLElement>
   }
