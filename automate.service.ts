62a877374b22b56cb7e504313e250f21136c6f0d
Michael Odumosu <21322737+MichaelOdumosu57@users.noreply.github.com>
2022-12-07 11:20:36 -0500

    Update automate.service.ts

--- automate.service.ts
+++ automate.service.ts
@@ -1,1 +1,30 @@
-[Environment]::SetEnvironmentVariable('path',"C:\Users\Restop-2345\unneeded\heroku\bin;"+ [Environment]::GetEnvironmentVariable('path', "Machine"),"Machine")
+  eventDispatcher(event: string, element: HTMLElement | Window | Element,keyboardCharCode:number =13) {
+
+    try {
+      let eventModern
+      if(["keydown","keyup"].includes(event) ){
+        let eventInitObj = {
+          13:{
+            code: 'Enter',
+            key: 'Enter',
+            charCode: 13,
+            keyCode: 13,
+            view: window,
+            bubbles: true
+          }
+        }[keyboardCharCode]
+        eventModern = new KeyboardEvent(event,eventInitObj)
+      }
+      else{
+
+        eventModern = new Event(event)
+      }
+
+      element.dispatchEvent(eventModern)
+    }
+    catch (e) {
+      let eventLegacy = document.createEvent("Event");
+      eventLegacy.initEvent(event, false, true);
+      element.dispatchEvent(eventLegacy)
+    }
+  }
