c123575d5538b6fd37b92fa9f2fe953bd302f5a6
Michael Odumosu <21322737+MichaelOdumosu57@users.noreply.github.com>
2023-01-19 23:02:39 -0500

    Update automate.service.ts

--- automate.service.ts
+++ automate.service.ts
@@ -1,4 +1,31 @@
-
+    {
+      "label": "pushing work to git remote",
+      "type": "shell",
+      "command": "git add .;git commit -m'[${input:pushing_work_to_git_remote0}] ${input:pushing_work_to_git_remote1}';git branch --unset-upstream;git push origin HEAD",
+      "group": "none",
+      "linux": {
+        "command": "git add .;git commit -m\"[${input:pushing_work_to_git_remote0}] ${input:pushing_work_to_git_remote1}\";git branch --unset-upstream;git push origin HEAD"
+      },
+      "presentation": {
+        "reveal": "always",
+        "panel": "new",
+        // "close": true
+      }
+    },
+       
+    {
+      "id": "pushing_work_to_git_remote0",
+      "description": "pushing_work_to_git_remote",
+      "default": "UPDATE",
+      "type": "pickString",
+      "options": ["UPDATE", "FIX", "PATCH", "BUG", "MERGE", "COMPLEX MERGE"]
+    },
+    {
+      "id": "pushing_work_to_git_remote1",
+      "description": "git commit desc",
+      "default": "additional work",
+      "type": "promptString"
+    },        
 
 eventDispatcher(event: string, element: HTMLElement | Window | Element,keyboardCharCode:number =13) {
 
